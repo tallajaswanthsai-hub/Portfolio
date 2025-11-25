cfae064abf8cd135f1fcf72be5b78e521302141b
jaswanth sai <tallajaswanthsai@gmail.com>
2025-11-25 21:24:53 +0530

    portfolio

--- src/components/HomeLcontent.jsx
+++ src/components/HomeLcontent.jsx
@@ -3,7 +3,7 @@ const Homelcontent = () => {
   return (
    
       <div className="   md:w-[50vw] ">
-     <img  className="  h-65 w-65  rounded-full bg-center object-cover   md:h-75 md:w-75 md:ml-15   lg:ml-30 lg:h-85 lg:w-85 xl:ml-30   2xl:ml-45  xl:m-32   " src="jash.jpg"/>
+     <img  className="  h-65 w-65  rounded-full bg-center object-cover   md:h-75 md:w-75 md:ml-15   lg:ml-30 lg:h-85 lg:w-85 xl:ml-30   2xl:ml-45  xl:m-32   " src="./jash.jpg"/>
         
      </div>
 
